a682f76d0cb378706ae062dfdf1cf55eb02659ac
Alex Bacart <13940752+alex-bacart@users.noreply.github.com>
2022-01-07 19:23:57 +0300

    Added new interfaces/traits

---
+++ src/Traits/SerializerAwareTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Bacart package.
+ *
+ * (c) Alex Bacart <alex@bacart.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bacart\SymfonyAware\Traits;
+
+use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
+use Symfony\Contracts\Service\Attribute\Required;
+
+trait SerializerAwareTrait
+{
+    protected SerializerInterface $serializer;
+
+    #[Required]
+    public function setSerializer(SerializerInterface $serializer): void
+    {
+        $this->serializer = $serializer;
+    }
+}
